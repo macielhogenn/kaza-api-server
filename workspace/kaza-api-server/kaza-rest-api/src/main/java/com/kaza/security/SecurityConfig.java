e8a527c3778913642b375b0b57c92abddd23323b
Maciel Hogenn <maciel.hog@gmail.com>
2016-09-30 20:14:54 -0300

    Add base project

---
+++ workspace/kaza-api-server/kaza-rest-api/src/main/java/com/kaza/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.kaza.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Created by Maciel on 04/08/2016.
+ */
+@EnableWebSecurity
+@Configuration
+@Slf4j
+class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated();
+
+    }
+
+}
+
